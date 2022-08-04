dd04286cbdf476255549e707d6fd9df5973f3cd0
phungvu17 <110510507+phungvu17@users.noreply.github.com>
2022-08-05 05:55:28 +0700

    Create 1.cpp
    bai tap 2

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	
+    int a;
+	printf("Nhap a=");
+	scanf("%d",&a);
+	
+	if(a>0){
+    printf("%d la so duong ",a);
+    
+	}else{
+	if(a<0);
+	printf("%d khong phai la so duong ",a); 	
+		
+	}
+	
+    
+    
+    
+    
+    
+    
+    
+    
+}	
