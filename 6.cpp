c6ff2c136bed39ccaddc982bd2114786d43c215d
phungvu17 <110510507+phungvu17@users.noreply.github.com>
2022-08-05 05:55:42 +0700

    Create 6.cpp
    bai tap 3

---
+++ 6.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main (){
+	int a;
+	printf("Nhap a=");
+	scanf("%d",&a);
+	
+	int b;
+	printf("Nhap b=");
+	scanf("%d",&b);
+	
+	if(a>b){
+	printf("%d  lon hon",a);
+	 
+    }else{
+    if(b>a)
+	printf("%d  lon hon",b);	
+	}
+
+	
+	
+	
+}
