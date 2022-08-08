38720c77dfb63a937669a81fe07bbcd86d479461
phungvu17 <110510507+phungvu17@users.noreply.github.com>
2022-08-08 16:39:11 +0700

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main (){
+	int n; 
+	printf ("nhap n =");
+	scanf("%d",&n);
+	int s= 0 ,i = 0 ;
+	
+	while(i<n){
+	
+	s=1+2+3+n;
+	i++;
+}
+	  printf("s la tong %d",s);
+	
+	
+	
+	
+	
+	
+	
+	
+}
