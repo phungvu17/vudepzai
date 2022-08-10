943cc1dcadfacbe15d0d18c98c7744f6a3a39403
phungvu17 <110510507+phungvu17@users.noreply.github.com>
2022-08-10 13:39:53 +0700

    Create bai tap 1.cpp

---
+++ bai tap 1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int n ;
+	printf("nhap n= ");
+	scanf("%d",&n);
+	int i = 1;
+	while(i<n){
+	
+     printf("  so le nho hon  %d",i);	
+		i+= 2 ;
+		
+		
+	}	
+	
+	
+
+
+	
+	
+	
+}
