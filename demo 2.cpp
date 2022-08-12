75a850264cac25760f69e69b76166841ac843ae4
phungvu17 <110510507+phungvu17@users.noreply.github.com>
2022-08-12 13:46:45 +0700

    Create demo 2.cpp

---
+++ demo 2.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main (){
+	int n,i,dem;
+	printf("nhap n =");
+	scanf("%d",&n);
+	
+	for(i=1;i<n;i++){
+	if (n%i==0){
+		printf ("\n %d chia het cho %d",n,i);
+		}dem++;
+	
+}
+	if(dem==2){
+		printf("\n%d la so nguyen to",n);
+		
+	}else{
+		printf("\n%d khong phai la so nguyen to ",n);
+	}{
+	
+	
+	}	
+		
+		
+
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
