03f744f7becfc9931a589893ea81f0f6821ad7aa
phungvu17 <110510507+phungvu17@users.noreply.github.com>
2022-08-22 06:23:02 +0700

    Create bai 1 buoi 4.cpp

---
+++ bai 1 buoi 4.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int n ,i;
+	printf("nhap n=");
+	scanf("%d",&n);
+	
+	for(i=1;i<n;i++){
+	if (n%i==0);
+	printf("\n %d chia het cho %d",n,i); 	
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
